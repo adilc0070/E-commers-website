2e1c4ed81f1e7b88ba3b64c875f7d0b5ef27d254
Adil <adilc0070@gmail.com>
2023-12-19 17:52:46 +0530

    last changes

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.get('*',(req,res)=>{
 
 
 // starting server
-app.listen(9100,()=>console.log('server started on http://localhost:9100'))
+app.listen(3000,()=>console.log('server started on http://localhost:3000'))
 
 
 
